8d4471ff94e86d95df5f8ac4ef1200584d282e5d
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2025-11-24 14:42:03 +0100

    Updated main branch

--- 00- Proyecto Final de Grado/MegustApp/app/src/main/java/com/example/megustapp/Register.java
+++ 00- Proyecto Final de Grado/MegustApp/app/src/main/java/com/example/megustapp/Register.java
@@ -1,6 +1,10 @@
 package com.example.megustapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,25 @@ public class Register extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        final TextView changeToLoginText = findViewById(R.id.change_to_login_text);
+        final Button restaurantRegisterButton = findViewById(R.id.restaurante_register_button);
+        final Button clientRegisterButton = findViewById(R.id.client_register_button);
+
+        changeToLoginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginActivity = new Intent(Register.this, Login.class);
+                startActivity(loginActivity);
+            }
+        });
+
+        restaurantRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
         });
     }
 }
