3da7937b43582d0272d324d6633f9377fc092406
samuelvergaramartin <samuelvergaramartin@protonmail.com>
2026-01-21 21:32:45 +0100

    Updated main branch

--- script.sql
+++ script.sql
@@ -5,19 +5,18 @@ use MegustApp;
 CREATE TABLE usuarios (
     id int auto_increment PRIMARY KEY,
     email VARCHAR(100) UNIQUE NOT NULL,
-    contraseña VARCHAR(100) NOT NULL
+    clave VARCHAR(100) NOT NULL
 );
 
 create table clientes (
 	id int auto_increment primary key,
     nombre varchar(50) not null,
     ciudad varchar(50) not null,
-    telefono varchar(15) not null,
+    telefono varchar(15) unique not null,
     numero_valoraciones int not null,
-    email varchar(50) not null,
+    email varchar(50) unique not null,
     clave varchar(100) not null,
-    foto_perfil longblob not null,
-    valoracion int
+    ruta_foto_perfil varchar(2048) not null
 );
 
 create table restaurantes(
@@ -26,23 +25,24 @@ create table restaurantes(
     telefono varchar(15) not null,
     ciudad varchar(50) not null,
     calle varchar(100) not null,
-    email varchar(50) not null,
+    email varchar(50) unique not null,
     clave varchar(100) not null,
-    foto_perfil longblob not null,
-    plato int
+    ruta_foto_perfil varchar(2048) not null
 );
 
 create table valoraciones (
     id int auto_increment primary key,
     nota int not null check (nota >= 1 and nota <= 5),
-    comentario text null
+    comentario text null,
+    cliente int not null
 );
 
 create table platos (
     id int auto_increment primary key,
     nombre varchar(50) not null,
     precio float(3,2) not null,
-    imagen longblob not null
+    ruta_imagen varchar(2048) not null,
+    restaurante int not null
 );
 
 create table ingredientes (
@@ -51,5 +51,13 @@ create table ingredientes (
     es_alergeno boolean not null default false
 );
 
-alter table clientes add constraint fk_valoracion foreign key (valoracion) references valoraciones(id);
-alter table restaurantes add constraint fk_plato foreign key (plato) references platos(id);
+create table plato_ingrediente (
+    id_plato int not null,
+    id_ingrediente int not null,
+    primary key(id_plato, id_ingrediente)
+);
+
+alter table platos add constraint fk_restaurante foreign key (restaurante) references restaurantes(id);
+alter table valoraciones add constraint fk_cliente foreign key (cliente) references clientes(id);
+alter table plato_ingrediente add constraint fk_pi_plato foreign key (id_plato) references platos(id);
+alter table plato_ingrediente add constraint fk_pi_ingrediente foreign key(id_ingrediente) references ingredientes(id);
